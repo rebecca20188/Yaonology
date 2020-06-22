494b55d4fd22748f14979947de95c37bfed7b888
rebecca20188 <45255154+rebecca20188@users.noreply.github.com>
2020-06-22 10:55:27 -0400

    Initial Commit

---
+++ YaonologyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YaonologyApp
+//
+//  Created by Rui Wang on 6/22/20.
+//  Copyright © 2020 Rui Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
